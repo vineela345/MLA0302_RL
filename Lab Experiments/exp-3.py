70362590e959fa932e959c609f51dc4032135697
vineela345 <vineelam42@gmail.com>
2026-08-04 14:50:05 +0530

    Add files via upload

---
+++ Lab Experiments/exp-3.py
@@ -0,0 +1,6 @@
+Python 3.13.7 (v3.13.7:bcee1c32211, Aug 14 2025, 19:10:51) [Clang 16.0.0 (clang-1600.0.26.6)] on darwin
+Enter "help" below or click "Help" above for more information.
+>>> 
+=================== RESTART: /Users/vinnu/Documents/exp-2.py ===================
+Total Revenue = 21262
+Selected Price = 18
